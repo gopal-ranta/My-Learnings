c9f050c564bf4b8870ae76e4828d80db556c5f0c
Gopal Ratna <gopal.ratna@aeris.net>
2020-02-15 12:40:06 -0600

    committing eureka server and other microservice

---
+++ sample-microservice-2/sample-microservice-2/src/main/java/eurekaserver/samplemicroservice2/SampleMicroservice2Application.java
@@ -0,0 +1,13 @@
+package eurekaserver.samplemicroservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMicroservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMicroservice2Application.class, args);
+	}
+
+}
---
+++ sample-microservice-2/sample-microservice-2/src/test/java/eurekaserver/samplemicroservice2/SampleMicroservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package eurekaserver.samplemicroservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMicroservice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
