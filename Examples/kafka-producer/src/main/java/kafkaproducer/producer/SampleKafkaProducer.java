12c9b8eca916fbff1fdeb75059bec6b7d831ff1b
Gopal Ratna <gopal.ratna@aeris.net>
2020-02-16 12:27:18 -0600

    added kafka producer acknowledgement policies

--- Examples/kafka-producer/src/main/java/kafkaproducer/producer/SampleKafkaProducer.java
+++ Examples/kafka-producer/src/main/java/kafkaproducer/producer/SampleKafkaProducer.java
@@ -1,10 +1,14 @@
 package kafkaproducer.producer;
 
 import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 public class SampleKafkaProducer {
 	public static void main(String[] args) {
@@ -21,10 +25,58 @@ public class SampleKafkaProducer {
 		Producer<String, String> producer = new KafkaProducer<>(props);
 		ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(topicName, key, value);
 		
+		/**
+		 * This approach of sending message is
+		 * called fire and forget approach
+		 */
 		producer.send(producerRecord);
 		
+		/**
+		 * This approach of sending message is
+		 * called synchronous approach,as you wait for
+		 * recordmetaData to come.
+		 */
+		try {
+			RecordMetadata recordMetaData = producer.send(producerRecord).get();
+			System.out.println("Partition :: " + recordMetaData.partition() + 
+					"Offset :: " + recordMetaData.offset());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/**
+		 * This approach of sending message is
+		 * called asynchronous approach,
+		 * you provide a callBack mechanism ater the message
+		 * is received successfully at kafka.
+		 */
+		MyCallBack myCallBack = new MyCallBack();
+		producer.send(producerRecord, myCallBack);
+		
+		/**
+		 * finally close the producer.
+		 */
 		producer.close();
 	}
 	
+	static class MyCallBack implements Callback{
+
+		@Override
+		public void onCompletion(RecordMetadata metadata, Exception exception) {
+			/*
+			 * In case of exception retry
+			 */
+			if (exception != null) {
+				//resend the messa
+			}
+			
+		}
+		
+		
+	}
 
 }
