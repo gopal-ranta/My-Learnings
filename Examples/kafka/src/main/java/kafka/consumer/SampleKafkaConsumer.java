d97bddbbdb7dc73c5f583529fbd48a550ad68ca9
Gopal Ratna <gopal.ratna@aeris.net>
2020-02-17 22:06:53 -0600

    modified the consumer code for adding the rebalance listener

--- Examples/kafka/src/main/java/kafka/consumer/SampleKafkaConsumer.java
+++ Examples/kafka/src/main/java/kafka/consumer/SampleKafkaConsumer.java
@@ -8,6 +8,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import kafka.customSerializer.MyCustomClass;
+import kafka.rebalanceListener.RebalanceListener;
 
 public class SampleKafkaConsumer {
 	
@@ -29,10 +30,26 @@ public class SampleKafkaConsumer {
 		KafkaConsumer<String, MyCustomClass> kafkaConsumer = new KafkaConsumer<>(props);
 		kafkaConsumer.subscribe(Arrays.asList(topicName));
 		
+		/*
+		 * instantiate a rebalance listener
+		 */
+		RebalanceListener rebalanceListener = new RebalanceListener(kafkaConsumer);
+		
 		while(true) {
 			ConsumerRecords<String, MyCustomClass> records = kafkaConsumer.poll(100);
 			for (ConsumerRecord<String, MyCustomClass> record : records) {
+				/**
+				 * Process the record
+				 */
 				System.out.println("Age :: " + record.value().getAge() + " Name :: " + record.value().getName());
+				
+				/**
+				 * If consumer goes down after processing the records and before committing the offset, 
+	   			   in that case we can use database to maintain the processed recods and committed offset and 
+	   			   make these two methods (processing the records and committing the offset) transactional. 
+				 */
+				
+				rebalanceListener.addCurrentOffset(record.topic(), record.partition(), record.offset());
 			}
 		}
 	}
